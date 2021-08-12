f3dbc7d63c9804f3d7fc7fa97bcb79aa41f46a1d
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-08-12 15:26:38 -0600

    challenge done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+
 
 function App() {
+
+  
+  const [event, setEvent] = useState([]);
+
+  
+  //1.GET event from API and sort it
+  useEffect(() => {
+    fetch('https://candidate.hubteam.com/candidateTest/v3/problem/dataset?userKey=fc1f41dec9b635995cefdcfdc971')
+      .then(res => res.json())
+      .then(event => {
+        setEvent(event.events);
+        event.sort((a, b) => a.visitorId - b.visitorId);
+      })
+      .catch(err => console.log(err));
+  }, [])
+
+  
+  
+  console.log(event)
+  
+
+  //POST data to another API
+  
+  useEffect(() => {
+    const sendData = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({title: 'Events'})
+    };
+    fetch('https://candidate.hubteam.com/candidateTest/v3/problem/result?userKey=fc1f41dec9b635995cefdcfdc971', sendData)
+      .then(res => res.json())
+      .then(event => this.setData({ event: event.id }))
+      .catch(err => console.log(err));
+  }, [])
+
+  console.log(event)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //Get data on the page
+
+    event.map((event, index) => {
+      console.log(event);
+      return (
+        <div className="event" key={index}>
+          <div className="container mx-auto px-auto">
+            <h1 className="timestamp">Visitor Id: {event.visitorId}</h1>
+            <h2 className="url">Timestamp: {event.timestamp}</h2>
+            <h3 className="url">URL: {event.url}</h3>
+          </div>
+        </div>
+      )
+    })
   );
-}
+  }
 
 export default App;
